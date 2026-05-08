d6cd4d897a7bdcf6bbe84f20d43d4abbbff406e3
Iti Tyagi <intechworx@gmail.com>
2026-05-08 16:30:33 +0200

    Adding lambda thread

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,21 @@ void test (int x) {
     std::cout << "argument passed is " << x << std::endl;
 }
 
+/* using the lamda function to create thread*/
+
+auto lambda=[](int x) {
+    std::cout << "inside the lambda function" << std::endl;
+    std::cout << "argument passed is " << x << std::endl;
+};
+
 int main() {
     std::thread mythread(&test, 100);
     mythread.join();
     std::cout <<"hello from the main thread!" <<std::endl;
+
+    std::thread mylamdathread(lambda,200);
+    mylamdathread.join();
+
+    std::cout << "the call after the second lambda thread" << std::endl;
     return 0;
 }
